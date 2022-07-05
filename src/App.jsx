4b91a317cbab7891fcf788e4af9faf91d7934e28
hefeng6500 <2443992009@qq.com>
2022-07-05 22:05:00 +0800

    feat: add redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Counter />
+    </>
+  );
+};
+
+export default App;
