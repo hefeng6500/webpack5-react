a60c6f7ac909a5e5b1b346aad69b08db1f307072
hefeng6500 <2443992009@qq.com>
2022-07-28 20:36:04 +0800

    fix

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -64,6 +64,7 @@ export const incrementAsync = createAsyncThunk(
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = (state: RootState) => state.counter.value;
+export const selectCount: (state: RootState) => number = (state: RootState) =>
+  state.counter.value;
 
 export default counterSlice.reducer;
