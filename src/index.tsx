96b0429726f411bad4450c3f3e5b95919c63ae15
hefeng6500 <2443992009@qq.com>
2022-07-29 12:04:07 +0800

    back

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import "@babel/polyfill";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./app";
+
+import store from "./store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>
+);
